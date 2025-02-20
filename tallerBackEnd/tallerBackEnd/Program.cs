34518a4fdf8f0f64bdf9b280dc85ced5abc618e6
jossueas <jossueas@gmail.com>
2025-02-19 18:44:12 -0600

    cambios con console

--- tallerBackEnd/tallerBackEnd/Program.cs
+++ tallerBackEnd/tallerBackEnd/Program.cs
@@ -6,6 +6,9 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("hola hola cambios1");
+            Console.WriteLine("hPrurdalakjflka");
+            Console.WriteLine("hola hola fdlkadfja
+            ");
         }
     }
 }
